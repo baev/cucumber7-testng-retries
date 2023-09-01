7d11759d1b9f6de80896aa34ed5a94d149ecdd51
Dmitry Baev <baev@users.noreply.github.com>
2023-09-01 15:59:19 +0100

    use bom

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,11 +39,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation("io.cucumber:cucumber-testng:$cucumberVersion")
-    testImplementation("io.cucumber:cucumber-java:$cucumberVersion")
+    testImplementation(platform("io.cucumber:cucumber-bom:$cucumberVersion"))
+    testImplementation("io.cucumber:cucumber-testng")
+    testImplementation("io.cucumber:cucumber-java")
 
-    testImplementation("io.qameta.allure:allure-cucumber7-jvm:$allureVersion")
-    testImplementation("io.qameta.allure:allure-testng:$allureVersion")
+    testImplementation(platform("io.qameta.allure:allure-bom:$allureVersion"))
+    testImplementation("io.qameta.allure:allure-cucumber7-jvm")
+    testImplementation("io.qameta.allure:allure-testng")
 
     testImplementation("org.slf4j:slf4j-simple:1.7.30")
 }
